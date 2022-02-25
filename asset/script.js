316db4ad6ad8a6288d7c0c400c5b7999c9e0bc1d
DESKTOP-O5C829I\mspr5 <hatsune161016@gmail.com>
2022-02-25 17:39:58 +0900

    編集しました。

--- asset/script.js
+++ asset/script.js
@@ -1,46 +1,110 @@
 $(function(){
 
-        $(".submit-btn").on("click",function(){
-          let input = $("#keyword").val();
-          let data = {
-            apikey:"guest",
-            keyword:input,
-            format:'jsonp'
+  // -------------------------------MIKU編集--------------------------------------------
+var count = 0
+  $('.bad').on('click',function(){
 
-          }
+    // count = count+1
+    count++
 
-        //ajaxの書き方
-        $.ajax({
-            url: 'https://sysbird.jp/toriko/api/', //アクセスするURL
-            type: 'get', //post or get
-            cache: false,        //cacheを使うか使わないかを設定
-            dataType:'jsonp',    //data type script・xmlDocument・jsonなど
-            data:data,           //アクセスするときに必要なデータを記載。パラメータ。連想配列で渡す。    
-          })
+    if(count == 1){
+    
+    //header,footerにローソンの装飾を追加/FamilyMartの装飾を削除
+    alert('そうですか。では、次回は是非他のお店を利用くださいませ(^-^)')
+    $(".header_color_famima").addClass('header_color_loson');
+    $(".famima").addClass('loson');
+    $(".header_color2_famima").addClass('header_color2_loson');
+    $(".footer_famima").addClass('footer_loson')
+    $('div').removeClass("header_color_famima");
+    $('div').removeClass("famima");
+    $('div').removeClass("header_color2_famima");
+    $('footer').removeClass("footer_famima");
+    $('.staff').addClass('staff_come');
 
-          .done(function(response) { 
-             //通信成功時の処理
-            //  console.log(response.item[0].name);
-            console.log(response.item[0].image);
-            let p = response.item[0].image
-            console.log(p);
-            $(".result").
-             //要素名.append()メソッドを使用するとうまくできる。
-             //成功したとき実行したいスクリプトを記載
-          })
+    }else if(count == 2){
 
-          .fail(function(xhr) {  
-            //通信失敗時の処理
-             //失敗したときに実行したいスクリプトを記載
-          })
+    //セブンに変更
+    alert('ローソンはお嫌いですか?')
+    $(".change").addClass('header_color_seven');
+    $(".change2").addClass('seven');
+    $(".change3").addClass('header_color2_seven');
+    $(".change4").addClass('footer_seven')
+    $('div').removeClass("header_color_loson");
+    $('div').removeClass("loson");
+    $('div').removeClass("header_color2_loson");
+    $('footer').removeClass("footer_loson");
+    $('.staff').removeClass('staff_come');
+    $('.nanako').addClass('nanako_walk');
 
-          .always(function(xhr, msg) { 
-            //通信完了時の処理
-            //結果に関わらず実行したいスクリプトを記載
-       });   
+
+    }else{
+    //FamilyMartに戻す
+    alert('おかえりなさい!あなたなら戻ってきてくれると信じてました!')
+    $(".change").addClass('header_color_famima');
+    $(".change2").addClass('famima');
+    $(".change3").addClass('header_color2_famima');
+    $(".change4").addClass('footer_famima')
+    $('div').removeClass("header_color_seven");
+    $('div').removeClass("seven");
+    $('div').removeClass("header_color2_seven");
+    $('footer').removeClass("footer_seven");
+    $('.thank').addClass('thank_pyon');
+    $('.nanako').removeClass('nanako_walk');
+    $('.famitiki').addClass('famitiki_dance');
+    }
+  })
+
+  $('.good').on('click',function(){
+
+    //感謝
+    $('.thank').toggleClass('thank_pyon');
+    $('.famitiki').toggleClass('famitiki_dance');
+  })
+
+// --------------------------------------------------------------------------------
+
+    //     $(".submit-btn").on("click",function(){
+    //       let input = $("#keyword").val();
+    //       let data = {
+    //         apikey:"guest",
+    //         keyword:input,
+    //         format:'jsonp'
+    //         order: r
+
+    //       }
+
+    //     //ajaxの書き方
+    //     $.ajax({
+    //         url: 'https://sysbird.jp/toriko/api/', //アクセスするURL
+    //         type: 'get', //post or get
+    //         cache: false,        //cacheを使うか使わないかを設定
+    //         dataType:'jsonp',    //data type script・xmlDocument・jsonなど
+    //         data:data,           //アクセスするときに必要なデータを記載。パラメータ。連想配列で渡す。    
+    //       })
+
+    //       .done(function(response) { 
+    //          //通信成功時の処理
+    //         //  console.log(response.item[0].name);
+    //         console.log(response.item[0].image);
+    //         let p = response.item[0].image
+    //         console.log(p);
+    //         $(".result").
+    //          //要素名.append()メソッドを使用するとうまくできる。
+    //          //成功したとき実行したいスクリプトを記載
+    //       })
+
+    //       .fail(function(xhr) {  
+    //         //通信失敗時の処理
+    //          //失敗したときに実行したいスクリプトを記載
+    //       })
+
+    //       .always(function(xhr, msg) { 
+    //         //通信完了時の処理
+    //         //結果に関わらず実行したいスクリプトを記載
+    //    });   
 
         
-    })
+    // })
 
  
 
